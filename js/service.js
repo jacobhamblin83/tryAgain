f28c064cb85596a5909e776e9bed5d0a4db3506a
Jacob Hamblin <jacobhamblin83@gmail.com>
2017-02-20 11:36:23 -0700

    fixed http

--- js/service.js
+++ js/service.js
@@ -2,7 +2,7 @@ angular.module('app').service('mainService', function($http){
     //base Url and Api keys set to variables
     var special = 'weather'
     var googleMapsBaseUrl = 'https://www.googleapis.com/geolocation/v1/geolocate?key='
-    var weatherBaseUrl = 'http://api.openweathermap.org/data/2.5/' + special + '?lat=';
+    var weatherBaseUrl = 'https://api.openweathermap.org/data/2.5/' + special + '?lat=';
     var googleMapsApiKey = 'AIzaSyCkC41MmHlVcU0o8Nx7_kpdebE4xbrhCaQ'
     var weatherApiKey = 'bb72eebad1cc851a1aacb694290588c1';
     //making lat and lon global i think...
